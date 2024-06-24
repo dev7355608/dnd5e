68c4e6657d8ef34d5929560767e38e4dbc472d4b
dev7355608 <dev7355608@gmail.com>
2024-06-24 20:29:08 +0200

    Don't allow Blindsight to detect burrowing creatures

--- module/canvas/detection-modes/blindsight.mjs
+++ module/canvas/detection-modes/blindsight.mjs
@@ -23,30 +23,23 @@ export class DetectionModeBlindsight extends DetectionMode {
 
   /** @override */
   _canDetect(visionSource, target) {
-    // Blindsight can detect anything.
+    if ( visionSource.object.document.hasStatusEffect(CONFIG.specialStatusEffects.BURROW) ) return false;
+    if ( target instanceof Token ) {
+      if ( target.document.hasStatusEffect(CONFIG.specialStatusEffects.BURROW) ) return false;
+    }
     return true;
   }
 
   /** @override */
   _testLOS(visionSource, mode, target, test) {
-    const polygonBackend = foundry.utils.isNewerVersion(game.version, 11)
-      ? CONFIG.Canvas.polygonBackends.sight
-      : CONFIG.Canvas.losBackend;
-    return !polygonBackend.testCollision(
+    return !CONFIG.Canvas.polygonBackends.sight.testCollision(
       { x: visionSource.x, y: visionSource.y },
       test.point,
       {
         type: "sight",
         mode: "any",
         source: visionSource,
-        // Blindsight is restricted by total cover and therefore cannot see
-        // through windows. So we do not want blindsight to see through
-        // a window as we get close to it. That's why we ignore thresholds.
-        // We make the assumption that all windows are configured as threshold
-        // walls. A move-based visibility check would also be an option to check
-        // for total cover, but this would have the undesirable side effect that
-        // blindsight wouldn't work through fences, portcullises, etc.
-        useThreshold: false
+        useThreshold: true
       }
     );
   }
